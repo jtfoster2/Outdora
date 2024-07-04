80f531b419d648a7d18128e43bb698b50de2cbfb
Nestor Afantchao <kokonestor@gmail.com>
2024-07-04 14:19:34 -0400

    Run local easy
    make the whole project easy to run locally.
    -change property to local and use use local property

--- src/main/kotlin/com/esep/outdoora/KotlinApplicationStarter.kt
+++ src/main/kotlin/com/esep/outdoora/KotlinApplicationStarter.kt
@@ -5,6 +5,7 @@ import org.springframework.boot.runApplication
 
 @SpringBootApplication
 class KotlinApplicationStarter
+
 fun main(args: Array<String>) {
-	runApplication<KotlinApplicationStarter>(*args)
+    runApplication<KotlinApplicationStarter>(*args)
 }
