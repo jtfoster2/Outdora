6d7f20dc486bfa40b11a218cc9e035c4150e53d5
John Foster <jtfoster2.phone@gmail.com>
2024-07-25 23:28:44 -0400

    Sprint 3 Unit/BDD Testing (#46)
    * Revert "Added home BDD test."
    
    This reverts commit f8a0e3dd0707d65255ed8ba1a01029fcd8e793be.
    
    * share controller tests
    
    * share controller tests

--- src/test/kotlin/com/esep/outdoora/cucumber/StepDefs.kt
+++ src/test/kotlin/com/esep/outdoora/cucumber/StepDefs.kt
@@ -1,6 +1,7 @@
 package com.esep.outdoora.cucumber
 
 import io.cucumber.java.en.And
+import io.cucumber.java.en.Given
 import io.cucumber.java.en.Then
 import io.cucumber.java.en.When
 import org.hamcrest.MatcherAssert
@@ -35,11 +36,5 @@ class StepDefs : SpringIntegrationTest() {
     @Throws(Throwable::class)
     fun the_client_receives_server_version_body(version: String) {
         MatcherAssert.assertThat(latestResponse!!.body, Matchers.`is`(version))
-    }
-
-    @And("^the client calls /$")
-    @Throws(Throwable::class)
-    fun the_client_visits_the_home_page() {
-        executeGet("http://localhost:8080/home")
     }
 }
--- src/test/kotlin/com/esep/outdoora/profile/CucumberIntegrationTest.kt
+++ src/test/kotlin/com/esep/outdoora/profile/CucumberIntegrationTest.kt
@@ -6,7 +6,7 @@ import org.junit.runner.RunWith
 
 
 @RunWith(Cucumber::class)
-@CucumberOptions(features = ["src/test/resources/features"])
+@CucumberOptions(features = ["src/test/resources"])
 class CucumberIntegrationTest
 
 
